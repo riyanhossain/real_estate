ead2dc5ec6e7840c72dabfc0ff2122059ff1a35f
mehrabriyan <mehraabriyan@gmail.com>
2022-03-02 12:58:45 +0600

    descrption added

--- src/components/PAGE2/Home1/H1Section/H1S2.js
+++ src/components/PAGE2/Home1/H1Section/H1S2.js
@@ -1,21 +1,156 @@
-import React from 'react'
-import { Col } from 'react-bootstrap'
+import { TextField } from "@mui/material";
+import React, { useState } from "react";
+import { Col } from "react-bootstrap";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import BathroomIcon from "@mui/icons-material/Bathroom";
+import BedroomParentIcon from "@mui/icons-material/BedroomParent";
+import KitchenIcon from "@mui/icons-material/Kitchen";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import './H1.css'
 
 function H1S2(props) {
-    const h1=props.h1
+  const { images, image } = props.h1;
+  let [imgQ, setImageQ] = useState(image.home1);
+  const handleClick = (i) => {
+    setImageQ(i);
+  };
   return (
-    <div style={{display: 'flex',alignItems:'center',justifyContent:'center'}}>
-        <div style={{width:'75%'}}>
-            <Col xs={8}>
-                <img src={props.h1.image} alt="" style={{width:'704px',height:'520px'}} />
-            </Col>
-            <Col xs={4}>
-            </Col>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{ width: "75%", display: "flex" }}>
+        <Col xs={8}>
+          <img src={imgQ} alt="" style={{ width: "704px", height: "520px" }} />
+          <div
+            style={{
+              width: "704px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-evenly",
+            }}
+          >
+            {images.map((i) => (
+              <img src={i} alt="" onClick={() => handleClick(i)} />
+            ))}
+          </div>
+          <div
+            style={{
+              height: "147px",
+              marginTop: "100px",
+              backgroundColor: "white",
+              borderRadius: "20px",
+            }}
+          >
+            <h4 style={{ paddingTop:'30px',marginLeft: '40px' }}>Details</h4>
+            <div
+              style={{
+                display: "flex",
+                marginTop: "50px",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <Col
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <DirectionsCarIcon />
+                <strong style={{ marginLeft: "5px" }}>{props.h1.car}</strong>
+              </Col>
+              <Col
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <BathroomIcon />
+                <strong style={{ marginLeft: "5px" }}>{props.h1.bath}</strong>
+              </Col>
+              <Col
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <BedroomParentIcon />
+                <strong style={{ marginLeft: "5px" }}>{props.h1.bed}</strong>
+              </Col>
+              <Col
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <KitchenIcon />
+                <strong style={{ marginLeft: "5px" }}>
+                  {props.h1.kitchen}
+                </strong>
+              </Col>
+              <Col
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <CalendarTodayIcon />
+                <strong style={{ marginLeft: "5px" }}>{props.h1.build}</strong>
+              </Col>
+            </div>
+          </div>
 
-        </div>
-        
+          <div
+            style={{
+              height: "394px",
+              marginTop: "15%",
+              backgroundColor: "white",
+              borderRadius: "20px",
+            }}
+          >
+            <div  >
+              <h4 className="desCrip">Description</h4>
+
+
+            <p className="desCrip">
+              At vero eos et iusto odio dignissimos ducimus, qui haec putat, ut
+              ipsi auctori huius disciplinae placet: constituam, quid sit
+              numeranda nec me ab illo inventore veritatis et expedita
+              distinctio nam libero tempore, cum memoriter, tum etiam ac
+              ratione.
+              <br />
+              <br />
+              Si sine metu degendae praesidia firmissima filium morte multavit
+              si sine causa? quae fuerit causa, mox videro; interea hoc tenebo,
+              si ob rem voluptas assumenda est, quid sit extremum et inter
+              mediocrem animadversionem atque natum sit, a natura incorrupte.
+              <br />
+              <br />
+              Omne animal, simul atque in sanguinem suum tam inportuno tamque
+              crudeli; sin, ut earum motus et accusamus et argumentandum et
+              dolore suo sanciret militaris imperii disciplinam exercitumque in
+              liberos atque haec ratio late patet in quo pertineant non possim.
+            </p>
+            </div>
+          </div>
+        </Col>
+        <Col xs={4}>
+          <TextField id="standard-basic" label="Standard" variant="standard" />
+          <TextField id="standard-basic" label="Standard" variant="standard" />
+          <TextField id="standard-basic" label="Standard" variant="standard" />
+          <TextField id="standard-basic" label="Standard" variant="standard" />
+        </Col>
+      </div>
     </div>
-  )
+  );
 }
 
-export default H1S2
+export default H1S2;
--- src/components/PAGE2/Home1/Home1.js
+++ src/components/PAGE2/Home1/Home1.js
@@ -2,15 +2,22 @@ import React from "react";
 import Footer from "../../Footer/Footer";
 import H1S1 from "./H1Section/H1S1";
 import H1S2 from "./H1Section/H1S2";
-import home1 from '../../../Images/grid1.png'
+import home1 from "../../../Images/grid1.png";
+import rec1 from "../../../Images/Rectangle1.png";
+import rec2 from "../../../Images/Rectangle2.png";
+import rec3 from "../../../Images/Rectangle3.png";
+import rec4 from "../../../Images/Rectangle4.png";
+import rec5 from "../../../Images/Rectangle5.png";
+import rec6 from "../../../Images/Rectangle6.png";
+import rec7 from "../../../Images/Rectangle7.png";
 
 
 function Home1() {
-  const h1 = 
-  {
+  const h1 = {
     id: "mato",
     title: "Mato House",
-    image: home1,
+    images: [ rec1, rec2, rec3, rec4, rec5 , rec6, rec7],
+    image: {home1},
     car: 3,
     bath: 2,
     bed: 3,
@@ -27,11 +34,10 @@ function Home1() {
   };
   return (
     <div>
-      <H1S1 h1={h1}/>
-      <H1S2 h1={h1}/>
-      <Footer/>
+      <H1S1 h1={h1} />
+      <H1S2 h1={h1} />
+      <Footer />
     </div>
-
   );
 }
 
